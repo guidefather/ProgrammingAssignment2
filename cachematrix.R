ce50bf0e4ba2d50f550e1c433248b15ea1940f9b
guidefather <guzowski@us.ibm.com>
2014-10-26 12:35:19 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## This function creates a special "matrix" object that can cache its inverse.
+#######################################################
+#  2014-10-26 TSG
+#
+#  Description
+#
+#     these functions cache the computation of the inverse of 
+#      a matrix and return the saved/cached value for the
+#      second and subsequent calls if the input has not
+#      changed
+#
+#  Usage 
+#     
+#    1. source this file  
+#    2. <variable, e.g., m > <- makeCacheMatrix(<input matrix> OR () )       
+#         if no input provided you must subsequenetly run m$set(<input matrix>)
+#    3. cacheSolve(m) 
+#          first and subsequent calls will return inverse matrix but subsequent calls with 
+#          same input will use cached value and print out a "getting cached data" message 
+#
+#  Assumptions 
+#  
+#     1.  input matrix is always invertible 
+#     2. no caching will be done for null/empty matrix
+#
+#######################################################
 
 makeCacheMatrix <- function(x = matrix()) {
-   
-   
-   
-   
-   
-   
-   
+
+# constructor function for cached inverse matrix calculation
+# containing functions = set, get, setimatrix, getimatrix
+
+        im <- matrix()
+        set <- function(y) {
+                x <<- y
+                im <<- matrix()
+        }
+        get <- function() x
+        setimatrix <- function(imatrix) im <<- imatrix
+        getimatrix <- function() im
+        list(set = set, get = get,
+             setimatrix = setimatrix,
+             getimatrix = getimatrix)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+# function to return computed or cached inverse matrix  
+# input is output of makeCacheMatrix function
+
+        im <- x$getimatrix()
+        if(!is.na(im[1,1])) {
+                message("getting cached data")
+                return(im)
+        }
+        data <- x$get()
+        im <- solve(data, ...)
+        x$setimatrix(im)
+        im
+
 }
